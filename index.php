5b093b8ab19db9c3cdc09b5fc7ad76e56da0c59e
prishpon <prishpon@gmail.com>
2024-07-05 11:54:29 +0200

    Adding shortcode for displaying programma page,adding programma static page as a function in shortcode

--- index.php
+++ index.php
@@ -9,9 +9,12 @@
 
 if( ! defined( 'ABSPATH' ) ) exit; 
 
+require_once plugin_dir_path(__FILE__) . 'inc/programmaHTML.php';
+
 class NiceEventsCalendarPlugin {
   function __construct() {
     add_action('wp_enqueue_scripts', array($this,'niceEventStyles'));
+    add_shortcode('event_calendar',array($this,'event_calendar'));
   }
 
 
@@ -19,6 +22,10 @@ class NiceEventsCalendarPlugin {
     wp_enqueue_script('nice_js', plugin_dir_url(__FILE__) . 'src/index.js', array('jquery'), '1.0', false);
     wp_enqueue_style('nice_styles', plugin_dir_url(__FILE__) . 'src/style.css');
 
+  }
+
+  function event_calendar(){
+    showProgrammaHTML();
   }
 
 }
